7d11764adce9ef931a994d1257c7adbcd37f429b
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-04-03 11:13:17 +0300

    Добавьте файлы проекта.

---
+++ Software.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num8
+{
+    abstract class Software
+    {
+        public abstract void Print();
+
+    }
+}
